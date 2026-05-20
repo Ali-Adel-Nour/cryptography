70db73c1f93ab52c8b17213b3d1d505d908a8d3d
Ali Nour Al Din <aliiiadel841@gmail.com>
2026-05-20 11:33:25 +0300

    feat: adding a comment for hmac function

--- KDFs/kdf.js
+++ KDFs/kdf.js
@@ -5,11 +5,13 @@ const iterations = 100000;
 const keyLength = 64; //512bits
 const digest = 'sha512';
 
-const blockIndex = Buffer.alloc(4);
-blockIndex.writeUInt32BE(1, 0); // Block index (1-based)
-const hmac = crypto.createHmac(digest, password)
-    .update(Buffer.concat([Buffer.from(salt, 'hex'), blockIndex]))
-    console.log("Hmac output:", hmac.digest('hex'));
+//this what happend behind the seen in pkdf2 function, it uses HMAC to generate the derived key, and it does this by iterating the process of hashing the password with the salt and the block index, and then using the output as the input for the next iteration. The final output is the derived key after the specified number of iterations.
+
+// const blockIndex = Buffer.alloc(4);
+// blockIndex.writeUInt32BE(1, 0); // Block index (1-based)
+// const hmac = crypto.createHmac(digest, password)
+//     .update(Buffer.concat([Buffer.from(salt, 'hex'), blockIndex]))
+//     console.log("Hmac output:", hmac.digest('hex'));
 
 crypto.pbkdf2(password, salt, iterations, keyLength, digest, (err, derivedKey) => {
     if(err) console.error(err);
